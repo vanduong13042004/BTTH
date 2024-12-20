09df70b1347591e557f184c7dbf0989ea995e315
duong130404 <nguyenvanduong130404@gmail.com>
2024-12-20 18:13:43 +0700

    Initial commit

--- Btapvenha/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-    <footer style="background-color: #f8f9fa; padding: 20px; text-align: center; margin-top: auto; border-top: 1px solid #eee;">
-        <div class="container">
-            <p>&copy; 2023 Trang Sản Phẩm</p>
-        </div>
-    </footer>
-</body>
-</html>
